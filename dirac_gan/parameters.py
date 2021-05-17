670515a1cde972531bf77ab4c91cf99397dd9c3e
EvgenyGorelik <74295459+EvgenyGorelik@users.noreply.github.com>
2021-05-17 17:17:35 +0200

    Initial Commit

---
+++ dirac_gan/parameters.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from collections import deque
+
+
+def f(t):
+    return -np.log(1+np.exp(-t))
+
+
+def df(t):
+    return (1+np.exp(t))**-1
+
+
+def D_phi(x,phi):
+    return phi*x
+
+
+def L(theta, phi):
+    return f(theta*phi) + f(0)
+
+
+def dL_theta(theta, phi):
+    return df(theta*phi)*phi
+
+
+def dL_phi(theta, phi):
+    return df(theta*phi)*theta
+
+
+def AGD_step(theta, phi, h):
+    theta -= h * dL_theta(theta,phi)
+    phi += h * dL_phi(theta,phi)
+    return theta, phi
+
+
+history_len = 100  # how many trajectory points to display
+
+h = 0.5
+theta = [np.random.rand()]
+phi = [np.random.rand()]
+
+for i in range(history_len):
+    theta_i, phi_i = AGD_step(theta[i],phi[i],h)
+    theta.append(theta_i)
+    phi.append(phi_i)
+
+fig = plt.figure(figsize=(5, 4))
+ax = fig.add_subplot(autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+ax.set_aspect('equal')
+ax.grid()
+ax.set_xlabel('\u03B8')
+ax.set_ylabel('\u03C6')
+
+ax.scatter([0],[0],c='k')
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], ',-', lw=1)
+time_template = 'time = %.1f'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+history_x, history_y = deque(maxlen=history_len), deque(maxlen=history_len)
+
+
+def animate(i):
+    current_theta, current_phi = theta[i], phi[i]
+
+    if i == 0:
+        history_x.clear()
+        history_y.clear()
+
+    history_x.append(current_theta)
+    history_y.append(current_phi)
+
+    line.set_data(current_theta, current_phi)
+    trace.set_data(history_x, history_y)
+    time_text.set_text(time_template % i)
+    return line, trace, time_text
+
+
+ani = FuncAnimation(fig, animate, 50, interval=100, blit=True)
+plt.show()
